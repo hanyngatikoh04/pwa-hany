0f5cc8235b27deaf9878c42b35c214a14715a517
Hany Ngatikoh <74999706+hanyngatikoh04@users.noreply.github.com>
2021-06-02 15:01:52 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,3 @@
-const fullname = document.getElementById("fullname");
-const headerFullname = document.getElementById("header-fullname");
-const headerHobby = document.getElementById("header-hobby");
-
-const fetchPromise = fetch("https://api-hany.herokuapp.com");
-fetchPromise.then(response => {
-  return response.json();
-}).then(response => {
-  var biography = response.results;
-  console.log(biography.hobi);
-  fullname.innerHTML = biography.nama;
-  headerFullname.innerHTML = biography.nama;
-  headerHobby.innerHTML = biography.hobi;
-});
-
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
