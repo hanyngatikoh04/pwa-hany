5b51a34d57fa0177ab6f1811ecde02c661c02fe6
Hany Ngatikoh <74999706+hanyngatikoh04@users.noreply.github.com>
2021-06-02 14:45:34 +0700

    Update serviceWorker.js

--- assets/js/serviceWorker.js
+++ assets/js/serviceWorker.js
@@ -1,17 +1,5 @@
 const assetBiography = "hany-biografi-assets";
-const assets = [
-  '/',
-  'index.html',
-  'assets/vendor/aos/aos.css',  
-  'assets/img/favicon.png',
-  'assets/img/apple-touch-icon.png',
-  'assets/vendor/bootstrap/css/bootstrap.min.css',
-  'assets/vendor/bootstrap-icons/bootstrap-icons.css',
-  'assets/vendor/boxicons/css/boxicons.min.css',
-  'assets/vendor/glightbox/css/glightbox.min.css',
-  'assets/vendor/swiper/swiper-bundle.min.css',
-  'assets/css/style.css'
-];
+const assets = [];
 
 self.addEventListener("install", event => {
   event.waitUntil(
