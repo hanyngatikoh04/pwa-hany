1a32eb8d175a192693f0ec0384179aa03b0bd378
Hany Ngatikoh <74999706+hanyngatikoh04@users.noreply.github.com>
2021-06-03 16:29:11 +0700

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,16 +3,31 @@ const assetBiography = "hany-biografi-assets";
 // konstanta berisi array asset yang akan di cache
 const assets = [
   '/',
-  'index.html',
-  'assets/vendor/aos/aos.css',  
+  'index.html',  
+  'portfolio-details.html',
   'assets/img/favicon.png',
   'assets/img/apple-touch-icon.png',
+  'assets/vendor/aos/aos.css',  
   'assets/vendor/bootstrap/css/bootstrap.min.css',
   'assets/vendor/bootstrap-icons/bootstrap-icons.css',
   'assets/vendor/boxicons/css/boxicons.min.css',
   'assets/vendor/glightbox/css/glightbox.min.css',
   'assets/vendor/swiper/swiper-bundle.min.css',
-  'assets/css/style.css'
+  'assets/css/style.css',
+  'assets/vendor/aos/aos.js',
+  'assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
+  'assets/vendor/glightbox/js/glightbox.min.js',
+  'assets/vendor/isotope-layout/isotope.pkgd.min.js',
+  'assets/vendor/swiper/swiper-bundle.min.js',
+  'assets/vendor/typed.js/typed.min.js',
+  'assets/vendor/waypoints/noframework.waypoints.js',
+  'assets/js/app.js',
+  'assets/js/main.js',
+  'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i',
+  'assets/img/profile-img.jpg',
+  'assets/img/portfolio/portfolio-details-1.jpg',
+  'assets/img/portfolio/portfolio-details-2.jpg',
+  'assets/img/portfolio/portfolio-details-3.jpg'
 ];
 
 // install service worker
